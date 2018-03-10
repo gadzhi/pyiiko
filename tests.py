492788a25989d18a02e901f4ce6ed9650897789e
Gadzhibala Pirmagomedov <gadzhi@yandex.ru>
2018-03-10 22:15:35 +0300

    Initial commit

--- tests.py
+++ tests.py
@@ -1,9 +1,12 @@
 from Pyiiko.server import IikoServer
 import requests
-i = IikoServer('operaderbent.iiko.it', '8080', 'admin', 'resto#test')
+#i = IikoServer('operaderbent.iiko.it', '8080', 'admin', 'resto#test')
 
-print(i.get_events(i.get_token()))
+#print(i.get_events(i.get_token()))
 
 
-re = requests.post('https://kurtuba.ru/create').content
-print(re)
+re1 = requests.post('http://localhost:3199/create').content
+string = {'name': 2}
+re = requests.get('http://localhost:3199/44c5afcd-a90a-420a-ab7c-2037ce2ee716/', string).content
+
+print(re1)
