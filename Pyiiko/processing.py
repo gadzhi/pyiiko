86b9ccd4f844ff48a2bdfd5649099352d65c1224
Gadzhibala Pirmagomedov <gadzhi@yandex.ru>
2018-03-13 19:44:45 +0300

    Добавлена ф-я получения списка id курьеров

--- Pyiiko/processing.py
+++ Pyiiko/processing.py
@@ -1,6 +1,10 @@
 import lxml
+import json
+import os
 '''ф-я обработки департаментов'''
 
+#with open('1.json', encoding='utf-8') as json_data:
+#    d = json.load(json_data)
 
 def dep_edit(xml):
     file = lxml.etree.fromstring(xml)
@@ -15,10 +19,21 @@ def dep_edit(xml):
 
     return departments
 
+
 def emp_edit(id_user, xml):
 
     file = lxml.etree.fromstring(xml)
-    user = file.xpath(r'.//id[text() = "{id}"]'r'/../name/text()'.format(id=id_user)
+    user = file.xpath(r'.//id[text() = "{id}"]'r'/../name/text()'.format(id=id_user))
 
     return user
+
+
+def courier_one(file):
+    couriers = []
+    for i in file['users']:
+
+        couriers.append(i['id'])
+
+    return couriers
+
 
