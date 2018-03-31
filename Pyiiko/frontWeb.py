2117c46668c2909e9a51681e1182c7a4f3ab858d
Gadzhibala Pirmagomedov <gadzhi@yandex.ru>
2018-03-31 20:43:50 +0300

    fix

--- Pyiiko/frontWeb.py
+++ Pyiiko/frontWeb.py
@@ -10,75 +10,65 @@ class FrontWebAPI:
 
     def token(self):
 
-        req = requests.get(
+        return requests.get(
             'http://' + self.url + "/api/login/" + self.moduleid).content
-        return req
+
 
     def quit(self, token):
         """Уничтожение токена"""
-        req = requests.get(
+        return requests.get(
             'http://' + self.url + "/api/logout/" + token).content
-        return req
 
     def orders(self, token):
         """Заказы"""
-        req = requests.get(
+        return requests.get(
             'http://' + self.url + "/api/orders?key=" + token,
             headers=self.content_type).content
-        return req
 
     def sections(self, token):
         """Отделения"""
-        req = requests.get(
+        return requests.get(
             'http://' + self.url + "/api/sections?key=" + token,
             headers=self.content_type).content
-        return req
 
     def tables(self, token):
         """Столы"""
-        req = requests.get(
+        return requests.get(
             'http://' + self.url + "/api/tables?key=" + token,
             headers=self.content_type).content
-        return req
 
     def products(self, token):
         """Продукты"""
-        req = requests.get(
+        return requests.get(
             'http://' + self.url + "/api/products?key=" + token,
             headers=self.content_type).content
-        return req
 
     def product_groups(self, token):
         """Иерархическое меню"""
-        req = requests.get(
+        return requests.get(
             'http://' + self.url + "/api/productgroups?key=" + token,
             headers=self.content_type).content
-        return req
 
     def quick_menu(self, token):
         """Быстрое меню для стола по умолчанию"""
-        req = requests.get(
+        return requests.get(
             'http://' + self.url + "/api/quickmenu?key=" + token,
             headers=self.content_type).content
-        return req
 
     def users(self, token):
         """Пользователи"""
-        req = requests.get(
+        return requests.get(
             'http://' + self.url + "/api/users?key=" + token,
             headers=self.content_type).content
-        return req
 
     def deliveries(self, token):
         """Доставки"""
-        req = requests.get(
+        return requests.get(
             'http://' + self.url + "/api/deliveries?key=" + token,
             headers=self.content_type).content
-        return req
 
     def kitchen(self, token):
         """Кухонные заказы"""
-        req = requests.get(
+        return requests.get(
             'http://' + self.url + "/api/kitchenorders?key=" + token,
             headers=self.content_type).content
-        return req
--- test/test_server.py
+++ test/test_server.py
@@ -1,8 +1,8 @@
-from .settings import iiko
+from test.settings import iiko
 
 def test_token():
     return iiko.token()
 
 
-g = iiko.products_find(token=test_token(), num=1)
+g = iiko.events(token=test_token(), revision=1)
 print(g)
